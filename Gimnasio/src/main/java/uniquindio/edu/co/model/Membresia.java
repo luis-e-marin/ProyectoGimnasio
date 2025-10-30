de8559795282859f8348e899c8882ce62d0b61fe
luis-e-marin <luise.marint@uqvirtual.edu.co>
2025-10-30 14:58:42 -0500

    .

--- Gimnasio/src/main/java/uniquindio/edu/co/model/Membresia.java
+++ Gimnasio/src/main/java/uniquindio/edu/co/model/Membresia.java
@@ -17,4 +17,54 @@ public class Membresia {
         this.estado = estado;
     }
 
+    @Override
+    public String toString() {
+        return "Membresia{" +
+                "tipoMembresia=" + tipoMembresia +
+                ", fechaInicio=" + fechaInicio +
+                ", fechaFin=" + fechaFin +
+                ", precio=" + precio +
+                ", estado=" + estado +
+                '}';
+    }
+
+    public TipoMembresia getTipoMembresia() {
+        return tipoMembresia;
+    }
+
+    public void setTipoMembresia(TipoMembresia tipoMembresia) {
+        this.tipoMembresia = tipoMembresia;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 }
