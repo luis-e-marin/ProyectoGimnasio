63b6674425d3960abd0016611df226d8723d2b51
luis-e-marin <luise.marint@uqvirtual.edu.co>
2025-10-30 21:21:09 -0500

    .

--- Gimnasio/src/main/java/uniquindio/edu/co/model/Usuario.java
+++ Gimnasio/src/main/java/uniquindio/edu/co/model/Usuario.java
@@ -6,10 +6,43 @@ private String identificacion;
 private int edad;
 private String telefono;
 
+public Usuario(String nombre, String identificacion, int edad, String telefono) {
+    this.nombre = nombre;
+    this.identificacion = identificacion;
+    this.edad = edad;
+    this.telefono = telefono;
+}
 
 
+    public String getNombre() { 
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getIdentificacion() {
+        return identificacion;
+    }
 
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
 
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
